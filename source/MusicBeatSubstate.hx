03e6042bdbfca8d34952730657b476bf68d56e6b
Bloxe <74748724+bloxee@users.noreply.github.com>
2023-01-31 22:22:38 -0600

    Music shit updated

--- source/MusicBeatSubstate.hx
+++ source/MusicBeatSubstate.hx
@@ -6,11 +6,6 @@ import flixel.FlxSubState;
 
 class MusicBeatSubstate extends FlxSubState
 {
-	public function new()
-	{
-		super();
-	}
-
 	private var curStep:Int = 0;
 	private var curBeat:Int = 0;
 	private var controls(get, never):Controls;
@@ -18,43 +13,51 @@ class MusicBeatSubstate extends FlxSubState
 	inline function get_controls():Controls
 		return PlayerSettings.player1.controls;
 
+	public function new()
+	{
+		super();
+	}
+
 	override function update(elapsed:Float)
 	{
-		// everyStep(); 
-		var oldStep:Int = curStep; 
-		updateCurStep(); 
-		curBeat = Math.floor(curStep / 4); 
-		if (oldStep != curStep && curStep >= 0) {
-			if (curStep >= 0) { 
-				stepHit(); 
-			} 
-		} 
+		var oldStep:Int = curStep;
+		updateCurStep();
+		curBeat = Math.floor(curStep / 4);
+		if (oldStep != curStep && curStep >= 0)
+		{
+			stepHit();
+		}
 		super.update(elapsed);
 	}
-		private function updateCurStep():Void
+
+	private function updateCurStep():Void
 	{
 		var lastChange:BPMChangeEvent = {
 			stepTime: 0,
 			songTime: 0,
 			bpm: 0
-		}
-		for (i in 0...Conductor.bpmChangeMap.length)
+		};
+		for (bpmChange in Conductor.bpmChangeMap)
 		{
-			if (Conductor.songPosition > Conductor.bpmChangeMap[i].songTime)
-				lastChange = Conductor.bpmChangeMap[i];
+			if (Conductor.songPosition >= bpmChange.songTime)
+			{
+				lastChange = bpmChange;
+			}
 		}
 
 		curStep = lastChange.stepTime + Math.floor((Conductor.songPosition - lastChange.songTime) / Conductor.stepCrochet);
 	}
 
-	public function stepHit():Void
+	private function stepHit():Void
 	{
 		if (curStep % 4 == 0)
+		{
 			beatHit();
+		}
 	}
 
-	public function beatHit():Void
+	private function beatHit():Void
 	{
-		//do literally nothing dumbass
+		// don't do shit here
 	}
 }
