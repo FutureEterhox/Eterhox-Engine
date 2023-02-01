8dc5085baf1c7b4d2cf5b45fb050d734b1276374
Bloxe <74748724+bloxee@users.noreply.github.com>
2023-01-31 23:39:17 -0600

    Song moment

--- source/Song.hx
+++ source/Song.hx
@@ -9,68 +9,59 @@ using StringTools;
 
 typedef SwagSong =
 {
-	var song:String;
-	var notes:Array<SwagSection>;
-	var bpm:Float;
-	var needsVoices:Bool;
-	var speed:Float;
+var song:String;
+var notes:Array<SwagSection>;
+var bpm:Float;
+var needsVoices:Bool;
+var speed:Float;
 
-	var player1:String;
-	var player2:String;
-	var validScore:Bool;
+var player1:String;
+var player2:String;
+var validScore:Bool;
 }
 
 class Song
 {
-	public var song:String;
-	public var notes:Array<SwagSection>;
-	public var bpm:Float;
-	public var needsVoices:Bool = true;
-	public var speed:Float = 1;
+public var song:String;
+public var notes:Array<SwagSection>;
+public var bpm:Float;
+public var needsVoices:Bool = true;
+public var speed:Float = 1;
 
-	public var player1:String = 'bf';
-	public var player2:String = 'dad';
+public var player1:String = 'bf';
+public var player2:String = 'dad';
 
-	public function new(song, notes, bpm)
-	{
-		this.song = song;
-		this.notes = notes;
-		this.bpm = bpm;
+public function new(song:String, notes:Array<SwagSection>, bpm:Float)
+{
+	this.song = song;
+	this.notes = notes;
+	this.bpm = bpm;
+}
+
+public static function loadFromJson(jsonInput:String, ?folder:String):SwagSong
+{
+	var path = '';
+	if (folder != null) {
+		path = folder.toLowerCase() + '/' + jsonInput.toLowerCase();
+	} else {
+		path = jsonInput.toLowerCase();
 	}
 
-	public static function loadFromJson(jsonInput:String, ?folder:String):SwagSong
+	var rawJson = Assets.getText(Paths.json(path)).trim();
+
+	while (!rawJson.endsWith("}"))
 	{
-		var rawJson = Assets.getText(Paths.json(folder.toLowerCase() + '/' + jsonInput.toLowerCase())).trim();
-
-		while (!rawJson.endsWith("}"))
-		{
-			rawJson = rawJson.substr(0, rawJson.length - 1);
-			// LOL GOING THROUGH THE BULLSHIT TO CLEAN IDK WHATS STRANGE
-		}
-
-		// FIX THE CASTING ON WINDOWS/NATIVE
-		// Windows???
-		// trace(songData);
-
-		// trace('LOADED FROM JSON: ' + songData.notes);
-		/* 
-			for (i in 0...songData.notes.length)
-			{
-				trace('LOADED FROM JSON: ' + songData.notes[i].sectionNotes);
-				// songData.notes[i].sectionNotes = songData.notes[i].sectionNotes
-			}
-
-				daNotes = songData.notes;
-				daSong = songData.song;
-				daBpm = songData.bpm; */
-
-		return parseJSONshit(rawJson);
+		rawJson = rawJson.substr(0, rawJson.length - 1);
+		// LOL GOING THROUGH THE BULLSHIT TO CLEAN IDK WHATS STRANGE
 	}
 
-	public static function parseJSONshit(rawJson:String):SwagSong
-	{
-		var swagShit:SwagSong = cast Json.parse(rawJson).song;
-		swagShit.validScore = true;
-		return swagShit;
-	}
+	return parseJSONshit(rawJson);
+}
+
+public static function parseJSONshit(rawJson:String):SwagSong
+{
+	var swagSong:SwagSong = cast Json.parse(rawJson).song;
+	swagSong.validScore = true;
+	return swagSong;
+}
 }
