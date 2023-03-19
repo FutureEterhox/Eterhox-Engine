f775102e7b55880ffe69646428281ed39e8dc0e7
Bloxe <74748724+bloxee@users.noreply.github.com>
2023-03-18 20:42:14 -0500

    FPS (working on it)

--- source/ui/PreferencesMenu.hx
+++ source/ui/PreferencesMenu.hx
@@ -1,5 +1,6 @@
 package ui;
 
+import openfl.events.Event;
 import openfl.Lib;
 import flixel.FlxG;
 import flixel.FlxObject;
@@ -7,137 +8,131 @@ import flixel.FlxCamera;
 import flixel.util.FlxColor;
 import haxe.ds.StringMap;
 
-class PreferencesMenu extends Page
-{
-	public static var preferences:StringMap<Dynamic> = new StringMap<Dynamic>();
+class PreferencesMenu extends Page {
 
-	var checkboxes:Array<CheckboxThingie> = [];
-	var menuCamera:FlxCamera;
-	var items:TextMenuList;
-	var camFollow:FlxObject;
+    public static var showFPS(default, null):Bool;
+    public static var preferences:StringMap<Dynamic> = new StringMap<Dynamic>();
 
-	override public function new()
-	{
-		super();
-		menuCamera = new FlxCamera();
-		FlxG.cameras.add(menuCamera, false);
-		menuCamera.bgColor = FlxColor.TRANSPARENT;
-		camera = menuCamera;
-		add(items = new TextMenuList());
-		createPrefItem('naughtyness', 'censor-naughty', true);
-		createPrefItem('downscroll', 'downscroll', false);
-		createPrefItem('flashing menu', 'flashing-menu', true);
-		createPrefItem('Camera Zooming on Beat', 'camera-zoom', true);
-		createPrefItem('FPS Counter', 'fps-counter', true);
-		createPrefItem('Auto Pause', 'auto-pause', false);
-		camFollow = new FlxObject(FlxG.width / 2, 0, 140, 70);
-		if (items != null)
-		{
-			camFollow.y = items.members[items.selectedIndex].y;
-		}
-		menuCamera.follow(camFollow, null, 0.06);
-		menuCamera.deadzone.set(0, 160, menuCamera.width, 40);
-		menuCamera.minScrollY = 0;
-		items.onChange.add(function(item:TextMenuItem)
-		{
-			camFollow.y = item.y;
-		});
-	}
+    var checkboxes:Array<CheckboxThingie> = [];
+    var menuCamera:FlxCamera;
+    var items:TextMenuList;
+    var camFollow:FlxObject;
 
-	public static function getPref(pref:String)
-	{
-		return preferences.get(pref);
-	}
+    public function new() {
+        super();
+        menuCamera = new FlxCamera();
+        FlxG.cameras.add(menuCamera, false);
+        menuCamera.bgColor = FlxColor.TRANSPARENT;
+        camera = menuCamera;
+        add(items = new TextMenuList());
+        createPrefItem('Profanity', 'censor-naughty', true);
+        createPrefItem('downscroll', 'downscroll', false);
+        createPrefItem('flashing menu', 'flashing-menu', true);
+        createPrefItem('Zoom on Beat', 'camera-zoom', true);
+        createPrefItem('FPS Counter', 'fps-counter', false);
+        createPrefItem('Auto Pause', 'auto-pause', false);
+        camFollow = new FlxObject(FlxG.width / 2, 0, 140, 70);
+        if (items != null) {
+            camFollow.y = items.members[items.selectedIndex].y;
+        }
+        menuCamera.follow(camFollow, null, 0.06);
+        menuCamera.deadzone.set(0, 160, menuCamera.width, 40);
+        menuCamera.minScrollY = 0;
+        items.onChange.add(function(item:TextMenuItem) {
+            camFollow.y = item.y;
+        });
+    }
 
-	public static function initPrefs()
-	{
-		preferenceCheck('censor-naughty', true);
-		preferenceCheck('downscroll', false);
-		preferenceCheck('flashing-menu', true);
-		preferenceCheck('camera-zoom', true);
-		preferenceCheck('fps-counter', true);
-		preferenceCheck('auto-pause', false);
-		if (!getPref('fps-counter'))
-		{
-			Lib.current.stage.removeChild(Main.fpsCounter);
-		}
-		FlxG.autoPause = getPref('auto-pause');
-	}
+    public static function getPref(pref:String):Dynamic {
+        return preferences.get(pref);
+    }
 
-	public static function preferenceCheck(identifier:String, defaultValue:Dynamic)
-	{
-		if (preferences.get(identifier) == null)
-		{
-			preferences.set(identifier, defaultValue);
-			trace('Set option!');
-		}
-		else
-		{
-			trace('found preference: ' + Std.string(preferences.get(identifier)));
-		}
-	}
+    public static function initPrefs() {
+        preferenceCheck('censor-naughty', true);
+        preferenceCheck('downscroll', false);
+        preferenceCheck('flashing-menu', true);
+        preferenceCheck('camera-zoom', true);
+        preferenceCheck('fps-counter', true);
+        preferenceCheck('auto-pause', true);
+        if (getPref('fps-counter')) {
+            Lib.current.stage.addChild(Main.fpsCounter);
+        }
+        FlxG.autoPause = getPref('auto-pause');
+    }
 
-	public function createPrefItem(label:String, identifier:String, value:Dynamic)
-	{
-		items.createItem(120, 120 * items.length + 30, label, Bold, function()
-		{
-			preferenceCheck(identifier, value);
-			if (Type.typeof(value) == TBool)
-			{
-				prefToggle(identifier);
-			}
-			else
-			{
-				trace('swag');
-			}
-		});
-		if (Type.typeof(value) == TBool)
-		{
-			createCheckbox(identifier);
-		}
-		else
-		{
-			trace('swag');
-		}
-		trace(Type.typeof(value));
-	}
+    public static function preferenceCheck(identifier:String, defaultValue:Dynamic) {
+        if (!preferences.exists(identifier)) {
+            preferences.set(identifier, defaultValue);
+            trace('set preference!');
+        } else {
+            trace('found preference: ' + Std.string(preferences.get(identifier)));
+        }
+    }
 
-	public function createCheckbox(identifier:String)
-	{
+    public function createPrefItem(label:String, identifier:String, value:Dynamic) {
+        var isBool = (Type.typeof(value) == TBool);
+        items.createItem(120, 120 * items.length + 30, label, Bold, function() {
+            preferenceCheck(identifier, value);
+            if (isBool) prefToggle(identifier);
+            else trace('swag');
+        });
+        if (isBool) createCheckbox(identifier);
+        trace(Type.typeof(value));
+    }
+
+	public function createCheckbox(identifier:String):Void {
 		var box:CheckboxThingie = new CheckboxThingie(0, 120 * (items.length - 1), preferences.get(identifier));
 		checkboxes.push(box);
 		add(box);
 	}
 
-	public function prefToggle(identifier:String)
-	{
+	public function prefToggle(identifier:String):Void {
 		var value:Bool = preferences.get(identifier);
 		value = !value;
 		preferences.set(identifier, value);
 		checkboxes[items.selectedIndex].daValue = value;
 		trace('toggled? ' + Std.string(preferences.get(identifier)));
-		switch (identifier)
-		{
+		switch (identifier) {
 			case 'auto-pause':
-				FlxG.autoPause = getPref('auto-pause');
+				FlxG.autoPause = preferences.get('auto-pause');
+				return;
 			case 'fps-counter':
-				if (getPref('fps-counter'))
-					Lib.current.stage.addChild(Main.fpsCounter);
-				else
-					Lib.current.stage.removeChild(Main.fpsCounter);
+				var prefValue:Bool = preferences.get('fps-counter');
+				if (value != prefValue) {
+					preferences.set(identifier, value);
+					checkboxes[items.selectedIndex].daValue = value;
+					trace('toggled? ' + Std.string(preferences.get(identifier)));
+					if (value) {
+						Lib.current.dispatchEvent(new Event("showFpsCounter"));
+					} else {
+						Lib.current.dispatchEvent(new Event("hideFpsCounter"));
+					}
+				}
+				return;								
 		}
-	}
-
-	override function update(elapsed:Float)
-	{
+	}		
+	
+	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
 		menuCamera.followLerp = CoolUtil.camLerpShit(0.05);
-		items.forEach(function(item:MenuItem)
-		{
-			if (item == items.members[items.selectedIndex])
+		items.forEach(function(item:MenuItem):Void {
+			if (item == items.members[items.selectedIndex]) {
 				item.x = 150;
-			else
+			} else {
 				item.x = 120;
+			}
 		});
+	}
+
+	public function setArg(arg0:String, isFalse:Bool):Void {}
+
+	public function set(arg0:String, isfalse:Bool):Void {}
+
+	public function getBool(arg0:String, isfalse:Bool):Bool {
+		throw new haxe.exceptions.NotImplementedException();
+	}
+
+	public function get(arg0:String) {
+		throw new haxe.exceptions.NotImplementedException();
 	}
 }
