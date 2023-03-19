9995dba9d2671a380282dea512467d91fbb37a3c
Bloxe <74748724+bloxee@users.noreply.github.com>
2023-03-18 20:21:03 -0500

    Main hx rework?
    nothing much really

--- source/Main.hx
+++ source/Main.hx
@@ -2,55 +2,87 @@ package;
 
 import flixel.FlxGame;
 import flixel.FlxState;
-import openfl.display.Sprite;
-import openfl.display.Stage;
+import openfl.Assets;
 import openfl.Lib;
+import openfl.display.FPS;
+import openfl.display.Sprite;
+import openfl.events.Event;
+import ui.PreferencesMenu;
+import openfl.display.StageScaleMode;
 
-class Main extends Sprite {
-    var gameWidth:Int = 1280;
-    var gameHeight:Int = 720;
-    var initialState:Class<FlxState> = TitleState;
-    var zoom:Float = -1;
-    var framerate:Int = 60;
-    var skipSplash:Bool = true;
-    var startFullscreen:Bool = false;
+class Main extends Sprite
+{
+	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
+	var zoom:Int = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+	var framerate:Int = 60; // How many frames per second the game should run at.
+	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
+	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 
-    public static var fpsCounter:openfl.display.FPS;
+	public static var fpsCounter:FPS;
 
-    public static function main():Void {
-        Lib.current.addChild(new Main());
-    }
+	// You can pretty much ignore everything from here on - your code should go in your states.
 
-    public function new() {
-        super();
-        addEventListener(openfl.events.Event.ADDED_TO_STAGE, init);
-    }
+	public static function main():Void
+	{
+		Lib.current.addChild(new Main());
+	}
 
-    private function init(e:openfl.events.Event):Void {
-        removeEventListener(openfl.events.Event.ADDED_TO_STAGE, init);
-        setupGame();
-    }
+	public function new()
+	{
+		super();
 
-    private function setupGame():Void {
-        var stageWidth:Int = Lib.current.stage.stageWidth;
-        var stageHeight:Int = Lib.current.stage.stageHeight;
-        if (zoom == -1) {
-            var ratioX:Float = stageWidth / gameWidth;
-            var ratioY:Float = stageHeight / gameHeight;
-            zoom = Math.min(ratioX, ratioY);
-            gameWidth = Math.ceil(stageWidth / zoom);
-            gameHeight = Math.ceil(stageHeight / zoom);
-        }
-        
+		if (stage != null)
+		{
+			init();
+		}
+		else
+		{
+			addEventListener(Event.ADDED_TO_STAGE, init);
+		}
+	}
+
+	private function init(?e:Event = null):Void
+		{
+			if (hasEventListener(Event.ADDED_TO_STAGE))
+			{
+				removeEventListener(Event.ADDED_TO_STAGE, init);
+			}
+		
+			#if !mobile
+			Main.fpsCounter = new FPS(10, 3, 0xFFFFFF);
+			Main.fpsCounter.defaultTextFormat = new openfl.text.TextFormat(null, null, null, true);
+			addChild(Main.fpsCounter);
+			Lib.current.stage.align = "tl";
+			Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+			if(Main.fpsCounter != null) {
+				Main.fpsCounter.visible = PreferencesMenu.showFPS;
+			}
+			#end
+		
+			setupGame();
+		}		
+
+	private function setupGame():Void
+	{
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (zoom == -1)
+		{
+			var ratioX:Float = stageWidth / gameWidth;
+			var ratioY:Float = stageHeight / gameHeight;
+			zoom = Math.ceil(Math.min(ratioX, ratioY));
+			gameWidth = Math.ceil(stageWidth / zoom);
+			gameHeight = Math.ceil(stageHeight / zoom);
+		}
+
+		#if !debug
+		initialState = TitleState;
+		#end
+
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, #if (flixel < "5.0.0") zoom, #end framerate, framerate, skipSplash, startFullscreen));                      
-        
-        #if !debug
-        initialState = TitleState;
-        #end
-
-        #if !mobile
-        fpsCounter = new openfl.display.FPS();                  
-        addChild(fpsCounter);
-        #end
-    }
+				
+	}
 }
