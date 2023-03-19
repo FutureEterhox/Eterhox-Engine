16f09d621b1a452a021cf00ba1eb744fce96d8f1
Bloxe <74748724+bloxee@users.noreply.github.com>
2023-03-18 20:41:35 -0500

    Power of what again

--- source/ui/OptionsMenu.hx
+++ source/ui/OptionsMenu.hx
@@ -2,54 +2,46 @@ package ui;
 
 import flixel.FlxG;
 
-class OptionsMenu extends Page
-{
-	var items:TextMenuList;
+class OptionsMenu extends Page {
+    var items:TextMenuList;
 
-	override public function new(showDonate:Bool)
-	{
-		super();
-		add(items = new TextMenuList());
-		createItem('preferences', function()
-		{
-			onSwitch.dispatch(PageName.Preferences);
-		});
-		createItem('controls', function()
-		{
-			onSwitch.dispatch(PageName.Controls);
-		});
-		if (showDonate)
-		{
-			createItem('donate', selectDonate, true);
-		}
-		createItem('exit', exit);
-	}
+    public function new(showDonate:Bool) {
+        super();
+        items = new TextMenuList();
+        add(items);
+        createItem('preferences', switchToPage(PageName.Preferences));
+        createItem('controls', switchToPage(PageName.Controls));
+        if (showDonate) createItem('donate', openDonatePage, true);
+        createItem('exit', exit);
+    }
 
-	public function createItem(label:String, callback:Dynamic, ?fireInstantly:Bool = false)
-	{
-		var item:TextMenuItem = items.createItem(0, 100 + 100 * items.length, label, Bold, callback);
-		item.fireInstantly = fireInstantly;
-		item.screenCenter(X);
-		return item;
-	}
+    private function createItem(label:String, callback:Dynamic, fireInstantly:Bool = false):TextMenuItem {
+        var item:TextMenuItem = items.createItem(0, 100 + 100 * items.length, label, Bold, callback);
+        item.fireInstantly = fireInstantly;
+        item.screenCenter(X);
+        return item;
+    }
 
-	override function set_enabled(state:Bool)
-	{
-		items.enabled = state;
-		return super.set_enabled(state);
-	}
+    override function set_enabled(state:Bool):Bool {
+        items.enabled = state;
+        return super.set_enabled(state);
+    }
 
-	public function hasMultipleOptions()
-	{
-		return items.length > 2;
-	}
+    public function hasMultipleOptions():Bool {
+        return items.length > 2;
+    }
 
-	function selectDonate()
-	{
-		#if linux
-		Sys.command('/usr/bin/xdg-open', ["https://github.com/bloxee/Eterhox-Engine", "&"]);
-		#else
-		FlxG.openURL('https://github.com/bloxee/Eterhox-Engine');
-		#end
-	}
+    private function switchToPage(pageName:PageName):Dynamic {
+        return function() {
+            onSwitch.dispatch(pageName);
+        };
+    }
+
+    private function openDonatePage():Void {
+        #if linux
+        Sys.command('/usr/bin/xdg-open', ["https://github.com/bloxee/Eterhox-Engine", "&"]);
+        #else
+        FlxG.openURL('https://github.com/bloxee/Eterhox-Engine');
+        #end
+    }
 }
